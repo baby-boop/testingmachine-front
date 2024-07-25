c4ec41313e2f8889d1230268c5a66b4edeab960c
БАТДЭЛГЭР АЛТАНЗУЛ <batdelger.a@interactive.local>
2024-07-25 18:21:08 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ModuleSelector from './components/ModuleSelector';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Module Selector</h1>
-        <ModuleSelector />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header" style={{
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundImage: `url("https://images.alphacoders.com/130/thumb-1920-1301370.png")` }}>
+          <h1>Автомат тестийн машин</h1>
+          <nav>
+            <Link to="/"></Link>
+          </nav>
+          <Routes>
+            <Route path="/" element={<ModuleSelector />} />
+          </Routes>
+        </header>
+        
+      </div>
+    </Router>
   );
 }
 
--- src/components/ModuleSelector.js
+++ src/components/ModuleSelector.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import '../index.css';
 
 function ModuleSelector() {
   const [selectedModule, setSelectedModule] = useState('');
   const [responseMessage, setResponseMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (e) => {
     setSelectedModule(e.target.value);
@@ -11,38 +13,57 @@ function ModuleSelector() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (selectedModule) {
-      axios.post('http://localhost:8282/api/execute', { module: selectedModule })
-        .then(response => {
-          setResponseMessage(response.data);
-        })
-        .catch(error => {
-          console.error('There was an error!', error);
-          setResponseMessage('Модуль ажиллуулахад алдаа гарлаа.');
-        });
+    if (!selectedModule) {
+      setResponseMessage('Модулиа сонгоно уу.');
+      return;
     }
+
+    setIsLoading(true);
+    setResponseMessage('');
+
+    axios.post('http://localhost:8282/api/execute', { module: selectedModule })
+      .then(response => {
+        setResponseMessage(response.data);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setResponseMessage('Модуль ажиллуулахад алдаа гарлаа.');
+        setIsLoading(false);
+      });
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Тест хийх модулиа сонгоно уу? 
-          <select value={selectedModule} onChange={handleChange}>
-            <option value="">...</option>
-            <option value="salary">Salary</option>
-            <option value="hr">HR</option>
-            <option value="request">Request</option>
-            <option value="contract">Contract</option>
-            <option value="task">Task</option>
-            <option value="supply">Supply</option>
-            <option value="store">Store</option>
-          </select>
-        </label>
-        <button type="submit">Илгээх</button>
-      </form>
-      {responseMessage && <p>{responseMessage}</p>}
-    </div>
+  <div className="p-3 max-w-md mx-auto bg-black bg-opacity-80 shadow-md rounded-lg">
+    <form onSubmit={handleSubmit} className="space-y-4">
+      <label className="block text-lg font-semibold text-white text-center">
+        Тест хийх модулиа сонгоно уу?
+        <select 
+          className="block w-full mt-2 p-3 border border-gray-300 rounded-md shadow-sm text-black focus:ring-blue-500 focus:border-blue-500"
+          value={selectedModule} onChange={handleChange}
+        >
+        <option value="" disabled>Модулиа сонгоно уу...</option>
+        <option value="salary">Цалингийн систем, Цаг бүртгэлийн систем</option>
+        <option value="hr">Core HR</option>
+        <option value="request">Зээлийн хүсэлтийн удирлага</option>
+        <option value="contract">Гэрээний удирдлага</option>
+        <option value="task">Ажил үүргийн систем</option>
+        <option value="supply">Худалдан авалтын удирдлага</option>
+        <option value="store">Дэлгүүрийн удирдлага</option>
+        <option value="strategic">Бизнес профайл</option>
+      </select>
+    </label>
+    <button 
+      type="submit" 
+      className={`button-19 w-full ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`} 
+      disabled={isLoading}
+    >
+      {isLoading ? 'Тестлэж байна...' : 'Тестлэх'}
+    </button>
+  </form>
+  {responseMessage && <p className="mt-1 text-red-500 text-center text-xl">{responseMessage}</p>}
+</div>
+  
+
   );
 }
 
