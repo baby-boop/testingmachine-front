cb1ad09f8f65f8fac53cc6d4aa6a6a84726421bc
БАТДЭЛГЭР АЛТАНЗУЛ <batdelger.a@interactive.local>
2024-07-30 10:58:17 +0800

    initil commit

--- src/components/ModuleSelector.js
+++ src/components/ModuleSelector.js
@@ -33,35 +33,35 @@ function ModuleSelector() {
   };
 
   return (
-  <div className="p-3 max-w-md mx-auto bg-black bg-opacity-80 shadow-md rounded-lg">
-    <form onSubmit={handleSubmit} className="space-y-4">
-      <label className="block text-lg font-semibold text-white text-center">
-        Тест хийх модулиа сонгоно уу?
-        <select 
-          className="block w-full mt-2 p-3 border border-gray-300 rounded-md shadow-sm text-black focus:ring-blue-500 focus:border-blue-500"
-          value={selectedModule} onChange={handleChange}
+    <div className="p-3 max-w-md mx-auto bg-black bg-opacity-80 shadow-md rounded-lg">
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <label className="block text-lg font-semibold text-white text-center">
+          Тест хийх модулиа сонгоно уу?
+          <select 
+            className="block w-full mt-2 p-3 border border-gray-300 rounded-md shadow-sm text-black focus:ring-blue-500 focus:border-blue-500"
+            value={selectedModule} onChange={handleChange}
+          >
+          <option value="" disabled>Модулиа сонгоно уу...</option>
+          <option value="salary">Цалингийн систем, Цаг бүртгэлийн систем</option>
+          <option value="hr">Core HR</option>
+          <option value="request">Зээлийн хүсэлтийн удирлага</option>
+          <option value="contract">Гэрээний удирдлага</option>
+          <option value="task">Ажил үүргийн систем</option>
+          <option value="supply">Худалдан авалтын удирдлага</option>
+          <option value="store">Дэлгүүрийн удирдлага</option>
+          <option value="strategic">Бизнес профайл</option>
+        </select>
+      </label>
+      <button 
+        type="submit" 
+        className={`button-19 w-full ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`} 
+        disabled={isLoading}
         >
-        <option value="" disabled>Модулиа сонгоно уу...</option>
-        <option value="salary">Цалингийн систем, Цаг бүртгэлийн систем</option>
-        <option value="hr">Core HR</option>
-        <option value="request">Зээлийн хүсэлтийн удирлага</option>
-        <option value="contract">Гэрээний удирдлага</option>
-        <option value="task">Ажил үүргийн систем</option>
-        <option value="supply">Худалдан авалтын удирдлага</option>
-        <option value="store">Дэлгүүрийн удирдлага</option>
-        <option value="strategic">Бизнес профайл</option>
-      </select>
-    </label>
-    <button 
-      type="submit" 
-      className={`button-19 w-full ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`} 
-      disabled={isLoading}
-    >
-      {isLoading ? 'Тестлэж байна...' : 'Тестлэх'}
-    </button>
-  </form>
-  {responseMessage && <p className="mt-1 text-red-500 text-center text-xl">{responseMessage}</p>}
-</div>
+        {isLoading ? 'Тестлэж байна...' : 'Тестлэх'}
+      </button>
+      </form>
+      {responseMessage && <p className="mt-1 text-red-500 text-center text-xl">{responseMessage}</p>}
+    </div>
   
 
   );
